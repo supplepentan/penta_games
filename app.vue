83f7c50e54ff82459bfa40ca6377fb48f69838bd
supplepentan <supplepentan@gmail.com>
2023-02-21 16:58:00 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<script setup>
+useHead({
+  title: "15puzzle",
+  meta: [
+    { charset: "utf-8" },
+    { name: "description", description: "Penta's Hubs" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+  ],
+  link: [
+    {
+      rel: "icon",
+      sizes: "32x32",
+      type: "image/png",
+      href: "/favicon.ico",
+    },
+  ],
+});
+</script>
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
---
+++ components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/fifteenthpuzzle">15puzzle</a>
+    <a href="/about">About</a>
+  </nav>
+</template>
